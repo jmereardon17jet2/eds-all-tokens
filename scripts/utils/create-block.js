9263b7ae97e7284e95d7330a84d241c4916a6718
Jamie Reardon <jamie.reardon@jet2.com>
2025-01-08 15:31:24 +0000

    add new block script

---
+++ scripts/utils/create-block.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const blockName = process.argv[2] || 'new-block';
+const BLOCK_DIR = `blocks/${blockName}`;
+const files = [`${BLOCK_DIR}/${blockName}.js`, `${BLOCK_DIR}/${blockName}.scss`];
+
+async function createDirectory(path) {
+  fs.mkdirSync(path, (err) => {
+    if (err) {
+      console.error(`Failed to create directory ${path}:`, err);
+      throw err;
+    }
+  });
+}
+
+function checkBlockExists() {
+  const blockExists = checkFilesExists();
+
+  if (blockExists) {
+    console.log(`Block already exists with name: ${blockName}`);
+    process.exit(0);
+  }
+}
+
+function checkFilesExists() {
+  return files.some(fs.existsSync);
+}
+
+function createBlockFiles() {
+  try {
+    createDirectory(`${BLOCK_DIR}`);
+
+    fs.readFile('scripts/utils/block.config.js', 'utf8', (err, js) => {
+      if (!err && js) {
+        fs.writeFile(`${BLOCK_DIR}/${blockName}.js`, js, (err) => {
+          if (err) {
+            console.error('Failed to create JS', err);
+            throw err;
+          }
+        });
+      }
+    });
+
+    fs.openSync(`${BLOCK_DIR}//${blockName}.scss`, 'a');
+
+    console.log(`Block: ${blockName} created!`);
+  } catch (err) {
+    console.error('Error creating block files:', err);
+  }
+}
+
+(async () => {
+  try {
+    checkBlockExists();
+    createBlockFiles();
+  } catch (err) {
+    console.error('Error creating block:', err);
+  }
+})();
