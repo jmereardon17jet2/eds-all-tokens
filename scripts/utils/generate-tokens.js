0468c001e1a2b7ade91c343b1c96c3dc92cadc53
Jamie Reardon <jamie.reardon@jet2.com>
2025-01-13 10:12:08 +0000

    update token helper to split out tokens

--- scripts/utils/generate-tokens.js
+++ scripts/utils/generate-tokens.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const CleanCSS = require('clean-css');
 const { globSync } = require('glob');
 const fs = require('fs');
@@ -5,28 +6,49 @@ const themeIndex = process.argv.indexOf('--theme');
 const themes = fs.readdirSync('node_modules/@jet2/designsystem.tokens/build/web');
 const options = require('./cleancss.config');
 let chosenTheme;
-let cssFiles;
 
 if (themeIndex > -1) chosenTheme = process.argv[themeIndex + 1];
 
-const createCSSFile = (theme) => {
-  cssFiles = globSync(`node_modules/@jet2/designsystem.tokens/build/web/${theme}/*/*.css`);
+const createCSSFiles = (theme) => {
+  const themePath = `node_modules/@jet2/designsystem.tokens/build/web/${theme}`;
+  const targetThemePath = `styles/themes/${theme}`;
 
-  if (!fs.existsSync('styles/themes')) fs.mkdirSync('styles/themes');
+  // Create theme directory if it doesn't exist
+  if (!fs.existsSync(targetThemePath)) fs.mkdirSync(targetThemePath, { recursive: true });
 
-  new CleanCSS(options).minify(cssFiles, function (error, output) {
-    if (error) {
-      console.log('Failed to compile CSS, check reference file path and that theme exists');
-      throw Error(error);
-    }
+  // Get all directories within the theme
+  const directories = fs.readdirSync(themePath);
 
-    fs.writeFile(`styles/themes/${theme}.css`, output.styles, () => true);
-    console.log(`Created tokens css file for ${theme}: styles/themes/${theme}.css`);
+  directories.forEach((dir) => {
+    const targetDirPath = path.join(targetThemePath, dir);
+
+    // Create directory for each subfolder in the theme
+    if (!fs.existsSync(targetDirPath)) fs.mkdirSync(targetDirPath, { recursive: true });
+
+    // Get all CSS files in the directory
+    const cssFiles = globSync(`${themePath}/${dir}/*.css`);
+
+    // Optimize and save each CSS file
+    cssFiles.forEach((file) => {
+      const fileName = path.basename(file);
+      const outputFilePath = path.join(targetDirPath, fileName);
+      const css = fs.readFileSync(file, 'utf8');
+
+      new CleanCSS(options).minify(css, (error, output) => {
+        if (error) {
+          console.error(`Failed to compile CSS: ${file}`, error);
+          return;
+        }
+
+        fs.writeFileSync(outputFilePath, output.styles);
+        console.log(`Created tokens CSS file: ${outputFilePath}`);
+      });
+    });
   });
 };
 
 if (chosenTheme) {
-  createCSSFile(chosenTheme);
+  createCSSFiles(chosenTheme);
 } else {
-  themes.forEach(createCSSFile);
+  themes.forEach(createCSSFiles);
 }
